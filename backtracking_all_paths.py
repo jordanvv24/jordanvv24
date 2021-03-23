801133f8a5e6948bc4c1622b2cc39e22c50e3054
jordanvv24 <73456962+jordanvv24@users.noreply.github.com>
2021-03-24 09:48:37 +1300

    Add files via upload

---
+++ backtracking_all_paths.py
@@ -0,0 +1,33 @@
+def all_paths(adj_list, source, destination):
+    paths = []
+    dfs_backtrack((source,), adj_list, paths)
+
+
+def dfs_backtrack(candidate, input_data, output_data):
+    if should_prune(candidate):
+        return
+    if is_solution(candidate, input_data):
+        add_to_output(candidate, output_data)
+    else:
+        for child_candidate in children(candidate, input_data):
+            dfs_backtrack(child_candidate, input_data, output_data)
+
+
+def add_to_output(candidate, output_data):
+    output_data.append(candidate)
+
+
+def should_prune(candidate):
+    return False
+
+
+def is_solution(candidate, input_data):
+    return len(candidate) == len(input_data)
+
+
+def children(candidate, input_data):
+    result = []
+    for i in input_data.difference(set(candidate)):
+        result.append(candidate + (i,))
+    return result
+
